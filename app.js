55dab1ea29f8aec05e210d979d7797e3ca812c38
Poppy Lewin <poppy@Poppys-Air.fritz.box>
2022-09-05 15:06:59 +0100

    parse code block in app removed as not needed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { getCategories } = require('./controllers/categories-controller');
 const app = express();
 
-app.use(express.json());
 
 app.get('/api/categories', getCategories)
 
